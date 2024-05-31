c900491bb57f28d24ed83530364df22ea80080e9
danamz31 <88967706+danamz31@users.noreply.github.com>
2024-05-31 09:48:54 +0400

    configuring the web API's security.

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins, 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers = ["*"],
+)
